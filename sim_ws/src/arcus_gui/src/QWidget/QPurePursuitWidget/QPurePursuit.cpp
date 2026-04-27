5a0d1b3ad55128188cee1327b30b0f26b4b20e64
gabduf <gab.duf04@icloud.com>
2026-04-27 14:44:27 -0400

    slider for ros2 param fix

--- sim_ws/src/arcus_gui/src/QWidget/QPurePursuitWidget/QPurePursuit.cpp
+++ sim_ws/src/arcus_gui/src/QWidget/QPurePursuitWidget/QPurePursuit.cpp
@@ -3,12 +3,12 @@
 QPurePursuitWidget::QPurePursuitWidget(std::shared_ptr<rclcpp::Node> node_, QWidget* parent_):
     QWidget(parent_),
     _node(node_),
-    _lookaheadSlider(parent_, node_),
-    _maxLookaheadSlider(parent_, node_),
-    _minLookaheadSlider(parent_, node_),
-    _speedLookeaheadGainSlider(parent_, node_),
-    _accelLatSlider(parent_, node_),
-    _accelLongSlider(parent_, node_)
+    _lookaheadSlider(parent_, node_, LOOKAHEAD_PARAM_NAME, PURE_PURSUIT_NODE_NAME, LOOKAHEAD_MIN, LOOKAHEAD_MAX, LOOKAHEAD_DEFAULT, LOOKAHEAD_PRECISION),
+    _maxLookaheadSlider(parent_, node_, MAX_LOOKAHEAD_PARAM_NAME, PURE_PURSUIT_NODE_NAME, MAX_LOOKAHEAD_MIN, MAX_LOOKAHEAD_MAX, MAX_LOOKAHEAD_DEFAULT, MAX_LOOKAHEAD_PRECISION),
+    _minLookaheadSlider(parent_, node_, MIN_LOOKAHEAD_PARAM_NAME, PURE_PURSUIT_NODE_NAME, MIN_LOOKAHEAD_MIN, MIN_LOOKAHEAD_MAX, MIN_LOOKAHEAD_DEFAULT, MIN_LOOKAHEAD_PRECISION),
+    _speedLookeaheadGainSlider(parent_, node_, SPEED_LOOKAHEAD_GAIN_PARAM_NAME, PURE_PURSUIT_NODE_NAME, SPEED_LOOKAHEAD_GAIN_MIN, SPEED_LOOKAHEAD_GAIN_MAX, SPEED_LOOKAHEAD_GAIN_DEFAULT, SPEED_LOOKAHEAD_GAIN_PRECISION),
+    _accelLatSlider(parent_, node_, ACCEL_LAT_PARAM_NAME, PURE_PURSUIT_NODE_NAME, ACCEL_LAT_MIN, ACCEL_LAT_MAX, ACCEL_LAT_DEFAULT, ACCEL_LAT_PRECISION),
+    _accelLongSlider(parent_, node_, ACCEL_LONG_PARAM_NAME, PURE_PURSUIT_NODE_NAME, ACCEL_LONG_MIN, ACCEL_LONG_MAX, ACCEL_LONG_DEFAULT, ACCEL_LONG_PRECISION)
 {
     _ui.setupUi(this);
     _ui.sliderLayout->addWidget(&_lookaheadSlider);
--- sim_ws/src/arcus_gui/src/QWidget/QPurePursuitWidget/QPurePursuit.hpp
+++ sim_ws/src/arcus_gui/src/QWidget/QPurePursuitWidget/QPurePursuit.hpp
@@ -11,6 +11,44 @@ class QPurePursuitWidget : public QWidget
 {
     Q_OBJECT
 
+    static constexpr const char* PURE_PURSUIT_NODE_NAME = "/arcus/pure_pursuit";
+
+    static constexpr const char* LOOKAHEAD_PARAM_NAME = "lookahead_distance";
+    static constexpr const char* MAX_LOOKAHEAD_PARAM_NAME = "max_lookahead_distance";
+    static constexpr const char* MIN_LOOKAHEAD_PARAM_NAME = "min_lookahead_distance";
+    static constexpr const char* SPEED_LOOKAHEAD_GAIN_PARAM_NAME = "speed_lookahead_gain";
+    static constexpr const char* ACCEL_LAT_PARAM_NAME = "accel_lat";
+    static constexpr const char* ACCEL_LONG_PARAM_NAME = "accel_long";
+
+    static constexpr float LOOKAHEAD_DEFAULT = 1.0;
+    static constexpr float MAX_LOOKAHEAD_DEFAULT = 5.0;
+    static constexpr float MIN_LOOKAHEAD_DEFAULT = 0.5;
+    static constexpr float SPEED_LOOKAHEAD_GAIN_DEFAULT = 0.5;
+    static constexpr float ACCEL_LAT_DEFAULT = 2.0;
+    static constexpr float ACCEL_LONG_DEFAULT = 2.0;
+
+    static constexpr float LOOKAHEAD_MIN = 0.0;
+    static constexpr float MAX_LOOKAHEAD_MIN = 0.0;
+    static constexpr float MIN_LOOKAHEAD_MIN = 0.0;
+    static constexpr float SPEED_LOOKAHEAD_GAIN_MIN = 0.0;
+    static constexpr float ACCEL_LAT_MIN = 0.0;
+    static constexpr float ACCEL_LONG_MIN = 0.0;
+
+
+    static constexpr float LOOKAHEAD_MAX = 5.0;
+    static constexpr float MAX_LOOKAHEAD_MAX = 10.0;
+    static constexpr float MIN_LOOKAHEAD_MAX = 10.0;
+    static constexpr float SPEED_LOOKAHEAD_GAIN_MAX = 20.0;
+    static constexpr float ACCEL_LAT_MAX = 20.0;
+    static constexpr float ACCEL_LONG_MAX = 20.0;
+
+    static constexpr float LOOKAHEAD_PRECISION = 0.1;
+    static constexpr float MAX_LOOKAHEAD_PRECISION = 0.1;
+    static constexpr float MIN_LOOKAHEAD_PRECISION = 0.1;
+    static constexpr float SPEED_LOOKAHEAD_GAIN_PRECISION = 0.1;
+    static constexpr float ACCEL_LAT_PRECISION = 0.1;
+    static constexpr float ACCEL_LONG_PRECISION = 0.1;
+
   public:
     QPurePursuitWidget(std::shared_ptr<rclcpp::Node> node_, QWidget* parent_);
 
