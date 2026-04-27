c60e7f50bb0fa7c04699fea0054642b5a1d92cb8
gabduf <gab.duf04@icloud.com>
2026-04-27 16:10:33 -0400

    finishing car info merging

--- sim_ws/src/arcus_gui/src/QWidget/QPurePursuitWidget/QPurePursuit.hpp
+++ sim_ws/src/arcus_gui/src/QWidget/QPurePursuitWidget/QPurePursuit.hpp
@@ -14,21 +14,21 @@ class QPurePursuitWidget : public QWidget
     static constexpr const char* PURE_PURSUIT_NODE_NAME = "/arcus/pure_pursuit";
 
     static constexpr const char* MAX_LOOKAHEAD_PARAM_NAME = "max_lookahead_distance_m";
-    static constexpr const char* MIN_LOOKAHEAD_PARAM_NAME = "min_lookahead_distance_m";
+    static constexpr const char* MAX_SPEED_PARAM_NAME = "speed_max";
     static constexpr const char* SPEED_LOOKAHEAD_GAIN_PARAM_NAME = "lookahead_distance_gain";
     static constexpr const char* ACCEL_LAT_PARAM_NAME = "a_lat_max";
     static constexpr const char* ACCEL_LONG_PARAM_NAME = "a_accel_max";
     static constexpr const char* BRAKE_LONG_PARAM_NAME = "a_brake_max";
 
-    static constexpr float MAX_LOOKAHEAD_DEFAULT = 5.0;
-    static constexpr float MIN_LOOKAHEAD_DEFAULT = 0.5;
-    static constexpr float SPEED_LOOKAHEAD_GAIN_DEFAULT = 0.5;
-    static constexpr float ACCEL_LAT_DEFAULT = 2.0;
-    static constexpr float ACCEL_LONG_DEFAULT = 2.0;
-    static constexpr float BRAKE_LONG_DEFAULT = 2.0;
+    static constexpr float MAX_LOOKAHEAD_DEFAULT = 3.5;
+    static constexpr float MAX_SPEED_DEFAULT = 8.0;
+    static constexpr float SPEED_LOOKAHEAD_GAIN_DEFAULT = 0.40;
+    static constexpr float ACCEL_LAT_DEFAULT = 2.4;
+    static constexpr float ACCEL_LONG_DEFAULT = 9.0;
+    static constexpr float BRAKE_LONG_DEFAULT = 7.5;
 
+    static constexpr float MAX_SPEED_MIN = 0.0;
     static constexpr float MAX_LOOKAHEAD_MIN = 0.0;
-    static constexpr float MIN_LOOKAHEAD_MIN = 0.0;
     static constexpr float SPEED_LOOKAHEAD_GAIN_MIN = 0.0;
     static constexpr float ACCEL_LAT_MIN = 0.0;
     static constexpr float ACCEL_LONG_MIN = 0.0;
@@ -36,18 +36,18 @@ class QPurePursuitWidget : public QWidget
 
 
     static constexpr float MAX_LOOKAHEAD_MAX = 10.0;
-    static constexpr float MIN_LOOKAHEAD_MAX = 10.0;
+    static constexpr float MAX_SPEED_MAX = 10.0;
     static constexpr float SPEED_LOOKAHEAD_GAIN_MAX = 20.0;
     static constexpr float ACCEL_LAT_MAX = 20.0;
     static constexpr float ACCEL_LONG_MAX = 20.0;
     static constexpr float BRAKE_LONG_MAX = 20.0;
 
-    static constexpr float MAX_LOOKAHEAD_PRECISION = 0.1;
-    static constexpr float MIN_LOOKAHEAD_PRECISION = 0.1;
-    static constexpr float SPEED_LOOKAHEAD_GAIN_PRECISION = 0.1;
-    static constexpr float ACCEL_LAT_PRECISION = 0.1;
-    static constexpr float ACCEL_LONG_PRECISION = 0.1;
-    static constexpr float BRAKE_LONG_PRECISION = 0.1;
+    static constexpr float MAX_LOOKAHEAD_PRECISION = 0.05;
+    static constexpr float MAX_SPEED_PRECISION = 0.05;
+    static constexpr float SPEED_LOOKAHEAD_GAIN_PRECISION = 0.05;
+    static constexpr float ACCEL_LAT_PRECISION = 0.05;
+    static constexpr float ACCEL_LONG_PRECISION = 0.05;
+    static constexpr float BRAKE_LONG_PRECISION = 0.05;
 
   public:
     QPurePursuitWidget(std::shared_ptr<rclcpp::Node> node_, QWidget* parent_);
@@ -58,7 +58,7 @@ class QPurePursuitWidget : public QWidget
     std::shared_ptr<rclcpp::Node> _node;
 
     QParamSlider _maxLookaheadSlider;
-    QParamSlider _minLookaheadSlider;
+    QParamSlider _speedMaxSlider;
     QParamSlider _speedLookeaheadGainSlider;
     QParamSlider _accelLatSlider;
     QParamSlider _accelLongSlider;
