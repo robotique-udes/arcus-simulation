4d765a4bde107bdf090a902db33ad242a3bc8584
gabduf <gab.duf04@icloud.com>
2026-05-10 16:15:42 -0400

    risk param in GUI

--- sim_ws/src/arcus_gui/src/QWidget/QMapRacelineHelpers/QMapRacelineHelpers.cpp
+++ sim_ws/src/arcus_gui/src/QWidget/QMapRacelineHelpers/QMapRacelineHelpers.cpp
@@ -3,8 +3,12 @@
 QMapRacelineHelpers::QMapRacelineHelpers(std::shared_ptr<rclcpp::Node> node_, QWidget* parent_):
     QWidget(parent_),
     _node(node_),
-    _racelineHelper(this, "Raceline Helpers", RACELINE_HELPER_CMD, true)
+    _racelineHelper(this, "Raceline Helpers", RACELINE_HELPER_CMD, true),
+    _ttcDecayRateSlider(parent_, node_, TTC_DECAY_RATE_PARAM_NAME, PURE_PURSUIT_NODE_NAME, TTC_DECAY_RATE_MIN, TTC_DECAY_RATE_MAX, TTC_DECAY_RATE_DEFAULT, TTC_DECAY_RATE_PRECISION),
+    _maxRiskSlider(parent_, node_, MAX_RISK_PARAM_NAME, MASTER_NODE_NAME, MAX_RISK_MIN, MAX_RISK_MAX, MAX_RISK_DEFAULT, MAX_RISK_PRECISION)
 {
     _ui.setupUi(this);
     _ui.buttonsLayout->addWidget(&_racelineHelper);
+    _ui.buttonsLayout->addWidget(&_ttcDecayRateSlider);
+    _ui.buttonsLayout->addWidget(&_maxRiskSlider);
 }
